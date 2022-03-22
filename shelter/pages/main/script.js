f79e06a39d399e8f9b7aa348248c775bc53f1f45
Kybik <82117054+kybikn@users.noreply.github.com>
2022-03-22 04:30:15 +0300

    feat: add interactive

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -6,32 +6,47 @@
 // </div>
 
 // Получить модальный
-var modal = document.getElementById("modal-content");
+// var modal = document.getElementById("modal-content");
 
 // Получить кнопку, которая открывает модальный
-var btn = document.getElementById("myBtn");
+// var btn = document.getElementById("card-button");
 
 // Получить элемент <span>, который закрывает модальный
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
 // Когда пользователь нажимает на кнопку, откройте модальный
-btn.onclick = function () {
+// btn.onclick = function () {
+//   modal.style.display = "block";
+// };
+
+// Когда пользователь нажимает на <span> (x), закройте модальное окно
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
+
+// Когда пользователь щелкает в любом месте за пределами модального, закройте его
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
+// =====================================
+
+const btnPrev = document.querySelector(".previous");
+const btnNext = document.querySelector(".next");
+let modal = document.querySelector(".no");
+let modal2 = document.querySelector(".yes");
+
+// btnPrev.addEventListener("click", prevSlide);
+// btnNext.addEventListener("click", nextSlide);
+
+btnNext.onclick = function () {
   modal.style.display = "block";
 };
 
-// Когда пользователь нажимает на <span> (x), закройте модальное окно
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// Когда пользователь щелкает в любом месте за пределами модального, закройте его
 window.onclick = function (event) {
-  if (event.target == modal) {
+  if (event.target == modal2) {
     modal.style.display = "none";
   }
 };
-// ======================================
-let btn = document.getElementById("our-friends-button");
-btn.onclick = function () {
-  modal.style.display = "block";
-};
+//   modal2.style.display = "none";
