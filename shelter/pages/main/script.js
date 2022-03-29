819e92d1111be3dda4e613b1bb1fa112372a7473
Kybik <82117054+kybikn@users.noreply.github.com>
2022-03-30 01:00:57 +0300

    feat: burger, pets modal

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -82,12 +82,23 @@
 
 // ----------------------hg------------------------
 const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".header-nav");
+// const logo = document.querySelector(".logo-title");
+
 hamburger.addEventListener("click", toggleMenu);
+nav.addEventListener("click", closeMenu);
+// logo.addEventListener("click", closeMenu);
+
 function toggleMenu() {
+  // logo.classList.toggle("navbar");
+  nav.classList.toggle("navbar");
   hamburger.classList.toggle("change");
 }
+
 function closeMenu(event) {
-  if (event.target.classList.contains("nav-link")) {
+  if (event.target.classList.contains("header-nav")) {
+    nav.classList.remove("navbar");
+    // logo.classList.remove("navbar");
     hamburger.classList.remove("change");
   }
 }
