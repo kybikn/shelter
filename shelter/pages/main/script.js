ce660c3f71cff077d2f18d58b37154a46ca2d037
Kybik <82117054+kybikn@users.noreply.github.com>
2022-04-18 23:12:45 +0300

    feat: add shadow

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -4,48 +4,69 @@ const nav = document.querySelector(".header-nav");
 const logo = document.querySelector(".logo");
 const header = document.querySelector(".header");
 const notOnly = document.querySelector(".not-only");
-const body = document.querySelector("body");
-// const scrollA = document.querySelector(".scroll");
-
-hamburger.addEventListener("click", toggleMenu);
-nav.addEventListener("click", closeMenu);
-// scrollA.addEventListener("click", scroll);
+const modal = document.querySelectorAll(".modal-block");
+const cardAll = document.querySelectorAll(".card");
+const shadow = document.querySelector(".shadow");
+// const body = document.querySelector("body");
+// const window = document.querySelector("window");
+// const cardButton = document.querySelectorAll(".card-button");
 
 function toggleMenu() {
-  // container.classList.toggle("active");
   hamburger.classList.toggle("active");
   nav.classList.toggle("active");
   logo.classList.toggle("active");
   header.classList.toggle("active");
   notOnly.classList.toggle("active");
-  body.classList.toggle("active");
-  // bodyScroll.classList.toggle("scroll");
+  shadow.classList.toggle("active");
+  // body.classList.toggle("active");
 }
 
 function closeMenu(event) {
   if (
     event.target.classList.contains("header-nav") ||
-    event.target.classList.contains("header-link")
+    event.target.classList.contains("header-link") ||
+    event.target.classList.contains("shadow")
   ) {
     hamburger.classList.remove("active");
     nav.classList.remove("active");
     logo.classList.remove("active");
     header.classList.remove("active");
     notOnly.classList.remove("active");
-    body.classList.remove("active");
-    // body.classList.toggle("target");
-    // bodyScroll.classList.remove("scroll");
-    // document.querySelector(".body").classList.remove("open");
+    shadow.classList.remove("active");
+    // body.classList.remove("active");
   }
 }
 
-// function scroll(event) {
-//   if (event.target.classList.contains("scroll")) {
-//     hamburger.classList.remove("active");
-//     notOnly.classList.remove("active");
-//     nav.classList.remove("active");
-//     logo.classList.remove("active");
-//     header.classList.remove("active");
+function openModal(event) {
+  let card = event.currentTarget.dataset.modal;
+  if (modal) {
+    location.href = card;
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+nav.addEventListener("click", closeMenu);
+shadow.addEventListener("click", closeMenu);
+cardAll.forEach((element) => element.addEventListener("click", openModal));
+// cardButton.forEach((element) => element.addEventListener("click", open));
+// cardAll.forEach((element) => element.addEventListener("click", open));
+// modal.forEach((element) => element.addEventListener("click", close));
+
+// window.addEventListener("click", open);
+// if (window.performance) {
+//   body.classList.add("active");
+// }
+
+// function open(event) {
+//   body.classList.add("active");
+// }
+
+// function close(event) {
+//   if (
+//     event.target.classList.contains("close-all") ||
+//     event.target.classList.contains("close-x") ||
+//     event.target.classList.contains("close")
+//   ) {
 //     body.classList.remove("active");
 //   }
 // }
--- shelter/pages/pets/script2.js
+++ shelter/pages/pets/script2.js
@@ -4,10 +4,11 @@ const nav = document.querySelector(".header-nav");
 const logo = document.querySelector(".logo");
 const header = document.querySelector(".header");
 const ourFriends = document.querySelector(".our-friends");
-const body = document.querySelector("body");
-
-hamburger.addEventListener("click", toggleMenu);
-nav.addEventListener("click", closeMenu);
+const modal = document.querySelectorAll(".modal-block");
+const cardAll = document.querySelectorAll(".card");
+const shadow = document.querySelector(".shadow");
+// const body = document.querySelector("body");
+// const cardButton = document.querySelectorAll(".card-button");
 
 function toggleMenu() {
   hamburger.classList.toggle("active");
@@ -15,19 +16,51 @@ function toggleMenu() {
   logo.classList.toggle("active");
   header.classList.toggle("active");
   ourFriends.classList.toggle("active");
-  body.classList.toggle("active");
+  shadow.classList.toggle("active");
+  // body.classList.toggle("active");
 }
 
 function closeMenu(event) {
   if (
     event.target.classList.contains("header-nav") ||
-    event.target.classList.contains("header-link")
+    event.target.classList.contains("header-link") ||
+    event.target.classList.contains("shadow")
   ) {
     hamburger.classList.remove("active");
     nav.classList.remove("active");
     logo.classList.remove("active");
     header.classList.remove("active");
-    notOnly.classList.remove("active");
-    body.classList.remove("active");
+    ourFriends.classList.remove("active");
+    shadow.classList.remove("active");
+    // body.classList.remove("active");
   }
 }
+
+function openModal(event) {
+  let card = event.currentTarget.dataset.modal;
+  if (modal) {
+    location.href = card;
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+nav.addEventListener("click", closeMenu);
+shadow.addEventListener("click", closeMenu);
+cardAll.forEach((element) => element.addEventListener("click", openModal));
+// cardButton.forEach((element) => element.addEventListener("click", open));
+// cardAll.forEach((element) => element.addEventListener("click", open));
+// modal.forEach((element) => element.addEventListener("click", close));
+
+// function open(event) {
+//   body.classList.add("active");
+// }
+
+// function close(event) {
+//   if (
+//     event.target.classList.contains("close-all") ||
+//     event.target.classList.contains("close-x") ||
+//     event.target.classList.contains("close")
+//   ) {
+//     body.classList.remove("active");
+//   }
+// }
