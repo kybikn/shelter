43d99fb7846ca99733c2680957d547be21eaf575
Kybik <82117054+kybikn@users.noreply.github.com>
2022-04-18 05:38:59 +0300

    feat: add body noscroll

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -4,30 +4,45 @@ const nav = document.querySelector(".header-nav");
 const logo = document.querySelector(".logo");
 const header = document.querySelector(".header");
 const notOnly = document.querySelector(".not-only");
-// const container = document.querySelector(".container");
+const body = document.querySelector("body");
+// const scrollA = document.querySelector(".scroll");
 
 hamburger.addEventListener("click", toggleMenu);
 nav.addEventListener("click", closeMenu);
-// nav1.addEventListener("click", closeMenu);
-// logo.addEventListener("click", closeMenu);
-
-// function start() {
-//   logo.classList.add("change");
-//   toggleMenu();
-// }
+// scrollA.addEventListener("click", scroll);
 
 function toggleMenu() {
   // container.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
+  logo.classList.toggle("active");
   header.classList.toggle("active");
   notOnly.classList.toggle("active");
-  logo.classList.toggle("active");
-  nav.classList.toggle("active");
-  hamburger.classList.toggle("active");
+  body.classList.toggle("active");
+  // bodyScroll.classList.toggle("scroll");
 }
 
 function closeMenu(event) {
   if (event.target.classList.contains("header-nav")) {
-    nav.classList.remove("active");
     hamburger.classList.remove("active");
+    nav.classList.remove("active");
+    logo.classList.remove("active");
+    header.classList.remove("active");
+    notOnly.classList.remove("active");
+    body.classList.remove("active");
+    // body.classList.toggle("target");
+    // bodyScroll.classList.remove("scroll");
+    // document.querySelector(".body").classList.remove("open");
   }
 }
+
+// function scroll(event) {
+//   if (event.target.classList.contains("scroll")) {
+//     hamburger.classList.remove("active");
+//     notOnly.classList.remove("active");
+//     nav.classList.remove("active");
+//     logo.classList.remove("active");
+//     header.classList.remove("active");
+//     body.classList.remove("active");
+//   }
+// }
--- shelter/pages/pets/script2.js
+++ shelter/pages/pets/script2.js
@@ -4,22 +4,27 @@ const nav = document.querySelector(".header-nav");
 const logo = document.querySelector(".logo");
 const header = document.querySelector(".header");
 const ourFriends = document.querySelector(".our-friends");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", toggleMenu);
 nav.addEventListener("click", closeMenu);
 
 function toggleMenu() {
-  ourFriends.classList.toggle("active");
-  header.classList.toggle("active");
-  logo.classList.toggle("active");
-  nav.classList.toggle("active");
   hamburger.classList.toggle("active");
+  nav.classList.toggle("active");
+  logo.classList.toggle("active");
+  header.classList.toggle("active");
+  ourFriends.classList.toggle("active");
+  body.classList.toggle("active");
 }
 
 function closeMenu(event) {
   if (event.target.classList.contains("header-nav")) {
+    hamburger.classList.remove("active");
     nav.classList.remove("active");
     logo.classList.remove("active");
-    hamburger.classList.remove("active");
+    header.classList.remove("active");
+    ourFriends.classList.remove("active");
+    body.classList.remove("active");
   }
 }
